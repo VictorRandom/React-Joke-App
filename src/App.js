a74bd6c33222b9704c7335bc1755687dffe5663d
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-14 10:23:02 -0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Joke from './components/Joke';
 
 function App() {
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Joke(props){
     return(
             <>
-                <div>Joke {props.number}</div>
-                <div>Setup: {props.setup}</div>
-                <div>Punchline: {props.punchline}</div>
-                <br></br>
+                {props.number && <h2>Joke {props.number}</h2>}
+                {props.setup && <h2>Setup: {props.setup}</h2>}
+                {props.punchline && <p>Punchline: {props.punchline}</p>}
+                <hr />
             </>
         )
 }
