e9eb22dcd046af6d34caaf20fe25212a4abd061b
Victor Miranda Balduino <victormbalduino@hotmail.com>
2022-01-17 11:13:32 -0300

    passando as jokes para o map

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import './App.css';
 import Joke from './components/Joke';
+import jokesData from './components/jokesData';
 
 function App() {
+  const jokeElements = jokesData.map((joke) => {
+    return <Joke number={joke.number} setup={joke.setup} punchline={joke.punchline} />
+  })
+
+  
+
   return (
-    <div className="App">
-      <Joke 
-      number="1"
-      setup="I got my daughter a fridge for her birthday"
-      punchline="I can't wait to see her face light up when she opens it." />
-      <Joke
-      number="2"     
-      setup="How did the hacker escape the police?"
-      punchline="He just ransomware!" />
-      <Joke
-      number="3"  
-      setup="Why don't pirates travel on mountain roads?"
-      punchline="Scurvy." />
-      <Joke    
-      number="4"    
-      setup="Why do bees stay in the hive in the winter?"
-      punchline="Swarm." />
-      <Joke      
-      number="5" 
-      setup="What's the best thing about Switzerland?"
-      punchline="I don't know, but the flag is a big plus!" />
+    <div>
+      { jokeElements }
     </div>
+
   );
 }
 
 export default App;
+
+
+
+//  <div className="App">
+// <Joke 
+// number="1"
+// setup="I got my daughter a fridge for her birthday"
+// punchline="I can't wait to see her face light up when she opens it." />
+// <Joke
+// number="2"     
+// setup="How did the hacker escape the police?"
+// punchline="He just ransomware!" />
+// <Joke
+// number="3"  
+// setup="Why don't pirates travel on mountain roads?"
+// punchline="Scurvy." />
+// <Joke    
+// number="4"    
+// setup="Why do bees stay in the hive in the winter?"
+// punchline="Swarm." />
+// <Joke      
+// number="5" 
+// setup="What's the best thing about Switzerland?"
+// punchline="I don't know, but the flag is a big plus!" /> 
+// </div>
+
 
 
 /* Challenge:
