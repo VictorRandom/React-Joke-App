0cddf59272a7626476de74f2527175e06448bd54
VictorRandom <victormbalduino@hotmail.com>
2022-06-23 11:06:33 -0300

    adding some ternary for the button

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -13,7 +13,7 @@ function Joke(props){
                 {props.number && <h2>Joke {props.number}</h2>}
                 {props.setup && <h2>Setup: {props.setup}</h2>}
                 {isShown ? props.punchline && <p>Punchline: {props.punchline}</p> : ""}
-                <button onClick={toggle}>Teste</button>
+                <button onClick={toggle}> {!isShown ? "Show " : "Hide "}Punchline</button>
                 <hr />
             </>
         )
